c8bd31b2dd34537e67ae090fda30ce6755035b34
Larry <larryzhi1230@gmail.com>
2020-05-30 12:15:19 -0700

    finished listviews w buttons

--- app/src/main/java/com/example/couchpotato/Activities/RecipeActivity.java
+++ app/src/main/java/com/example/couchpotato/Activities/RecipeActivity.java
@@ -22,6 +22,8 @@ import com.example.couchpotato.Classes.Recipe;
 import com.example.couchpotato.MainActivity;
 import com.example.couchpotato.R;
 
+import org.w3c.dom.Text;
+
 import java.security.cert.LDAPCertStoreParameters;
 import java.util.ArrayList;
 
@@ -59,61 +61,106 @@ public class RecipeActivity extends AppCompatActivity {
         store = new ArrayList<>();
 
 
-        store.add(new Recipe(ingredients, "pasta", "Italian"));
-        store.add(new Recipe(ingredients, "noodles", "Chinese"));
-        store.add(new Recipe(ingredients, "curry", "India"));
-        store.add(new Recipe(ingredients, "baguettes", "French"));
-        store.add(new Recipe(ingredients, "rice", "Chinese"));
-        store.add(new Recipe(ingredients, "beer", "Germany"));
-        store.add(new Recipe(ingredients, "pasta", "Italian"));
-        store.add(new Recipe(ingredients, "pasta", "Italian"));
-        store.add(new Recipe(ingredients, "pasta", "Italian"));
-        store.add(new Recipe(ingredients, "pasta", "Italian"));
-        store.add(new Recipe(ingredients, "pasta", "Italian"));
+        store.add(new Recipe(ingredients, "pasta", "Italian", 1));
+        store.add(new Recipe(ingredients, "noodles", "Chinese", 1));
+        store.add(new Recipe(ingredients, "curry", "India", 10));
+        store.add(new Recipe(ingredients, "baguettes", "French", 1));
+        store.add(new Recipe(ingredients, "rice", "Chinese", 20));
+        store.add(new Recipe(ingredients, "pasta", "Italian", 1));
+        store.add(new Recipe(ingredients, "noodles", "Chinese", 1));
+        store.add(new Recipe(ingredients, "curry", "India", 10));
+        store.add(new Recipe(ingredients, "baguettes", "French", 1));
+        store.add(new Recipe(ingredients, "rice", "Chinese", 20));
+
 
 
         final ArrayList <String> recipeNames = new ArrayList<>();
-        ArrayList <String> recipePreferences = new ArrayList<>();
+        ArrayList <Double> recipePrice = new ArrayList<>();
+        ArrayList <String> recipeAmount = new ArrayList<>();
         for (Recipe r : store){
             recipeNames.add(r.getName());
-            recipePreferences.add(r.getPreference());
+            recipePrice.add(r.getPrice());
+            recipeAmount.add("2");
         }
 
-        MyAdapter adapter = new MyAdapter(this, recipeNames, recipePreferences);
+        MyAdapter adapter = new MyAdapter(this, recipeNames, recipePrice);
         listView.setAdapter(adapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(RecipeActivity.this, "Added to Cart", Toast.LENGTH_SHORT).show();
-                //add to users cart TODO:
-            }
-        });
 
     }
 
     class MyAdapter extends ArrayAdapter<String> {
         Context context;
         ArrayList <String> recipeNames;
-        ArrayList <String> recipePreferences;
+        ArrayList <Double> recipePrice;
+        ArrayList <String> recipeAmount;
+        Button up;
+        Button down;
+        Button toCart;
+        TextView countIng;
 
-        MyAdapter (Context c, ArrayList<String> names, ArrayList<String> preferences){
+
+        MyAdapter (Context c, ArrayList<String> names, ArrayList<Double> price){
             super(c, R.layout.row, R.id.main_title, names);
             this.context = c;
             this.recipeNames = names;
-            this.recipePreferences = preferences;
+            this.recipePrice = price;
         }
 
         @NonNull
         @Override
-        public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        public View getView(final int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+
             LayoutInflater layoutInflater = (LayoutInflater)getApplicationContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View row = layoutInflater.inflate(R.layout.row, parent, false);
             TextView myTitle;
             myTitle = row.findViewById(R.id.main_title);
             TextView myDescription = row.findViewById(R.id.sub_title);
             myTitle.setText(recipeNames.get(position));
-            myDescription.setText(recipePreferences.get(position));
+            myDescription.setText("$" + recipePrice.get(position).toString());
+
+            toCart = row.findViewById(R.id.to_cart);
+            up = row.findViewById(R.id.button_up);
+            down = row.findViewById(R.id.button_down);
+            countIng = row.findViewById(R.id.count);
+
+
+            countIng.setText("0");
+
+            up.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    countIng = listView.getChildAt(position).findViewById(R.id.count);
+                    Integer a = Integer.parseInt(countIng.getText().toString());
+                    a++;
+                    countIng.setText(a.toString());
+                    return;
+                }
+            });
+
+            down.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    countIng = listView.getChildAt(position).findViewById(R.id.count);
+                    String s = countIng.getText().toString();
+                    Integer a = Integer.parseInt(s) - 1;
+                    if (a<0){
+                        return;
+                    }
+                    countIng.setText(a.toString());
+                    return;
+                }
+            });
+
+            toCart.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(RecipeActivity.this, "Added to Cart", Toast.LENGTH_SHORT).show();
+                    //add to users cart TODO:
+                    return;
+                }
+            });
 
             return row;
         }
